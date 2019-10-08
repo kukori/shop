c14b79546c4c1058b81d0719539bb0c5991dbb03
attilav <attila.varjas@gmail.com>
2019-10-08 08:42:04 +0700

    shop page, collection preview, collection item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <Router>
             <Switch>
                 <Route exact path='/' component={HomePage} />
+                <Route exact path='/shop' component={ShopPage} />
             </Switch>
         </Router>
     );
